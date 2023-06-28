96e74f2e2c16bed6c9448c472911cba85972f830
Mnishthakur <102964888+Mnishthakur@users.noreply.github.com>
2023-06-28 15:10:11 +0530

    [Manish Thakur]: Added 2nd program

--- Program.cs
+++ Program.cs
@@ -1,46 +1,64 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Collection_Problem
 {
-    public class AmeliaBookReader
+    public class FruitSeller
     {
-        public static int GetChaptersYetToRead(int N, List<Tuple<int, int>> chapters, int K)
+        public static int MaximizeSellingPrice(int N, int K, List<int> basketA, List<int> basketB)
         {
-            int chaptersYetToRead = 0;
-
-            foreach (var chapter in chapters)
+            // Check for invalid inputs
+            if (N <= 0 || K < 0 || basketA.Count != N || basketB.Count != N)
             {
-                int startPage = chapter.Item1;
-                int endPage = chapter.Item2;
-
-                if (startPage <= K && K <= endPage)
-                    break;
-
-                chaptersYetToRead++;
+                throw new ArgumentException("Invalid input");
             }
 
-            return N - chaptersYetToRead;
+            // Sort basket A in ascending order
+            basketA.Sort();
+
+            // Sort basket B in descending order
+            basketB.Sort();
+            basketB.Reverse();
+
+            // Perform at most K swaps
+            for (int i = 0; i < K; i++)
+            {
+                if (i < N && basketA[i] < basketB[i])
+                    basketA[i] = basketB[i];
+                else
+                    break;
+            }
+
+            // Calculate the total selling price of basket A
+            int totalPrice = basketA.Sum();
+
+            return totalPrice;
         }
 
         public static void Main()
         {
-            int N = 4; // Total number of chapters
-            List<Tuple<int, int>> chapters = new List<Tuple<int, int>>()
-        {
-            Tuple.Create(1, 4),
-            Tuple.Create(5, 7),
-            Tuple.Create(9, 16),
-            Tuple.Create(17, 26)
-        };
-            int K = 7; // Current reading page
+            try
+            {
+                // Read input
+                Console.WriteLine("Enter the number of fruits in each basket (N K):");
+                string[] input1 = Console.ReadLine().Split(' ');
+                int N = int.Parse(input1[0]); // Number of fruits in each basket
+                int K = int.Parse(input1[1]); // Maximum number of swaps
 
-            int chaptersYetToRead = GetChaptersYetToRead(N, chapters, K);
-            Console.WriteLine("Number of chapters yet to read: " + chaptersYetToRead);
+                Console.WriteLine("Enter the selling prices of fruits in basket A:");
+                List<int> basketA = Console.ReadLine().Split(' ').Select(int.Parse).ToList(); // Selling prices of fruits in basket A
+
+                Console.WriteLine("Enter the selling prices of fruits in basket B:");
+                List<int> basketB = Console.ReadLine().Split(' ').Select(int.Parse).ToList(); // Selling prices of fruits in basket B
+
+                int maxSellingPrice = MaximizeSellingPrice(N, K, basketA, basketB);
+                Console.WriteLine("The maximum selling price of the basket A: " + maxSellingPrice);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
         }
     }
-
 }
-
-
-
